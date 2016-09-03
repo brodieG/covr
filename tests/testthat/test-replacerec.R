e0bc16126b8bc57bdb42cb0d8b9865eff9169598
brodieG <brodieG@users.noreply.github.com>
2016-09-03 15:42:05 -0400

    comment out non-working tests

--- tests/testthat/test-replacerec.R
+++ tests/testthat/test-replacerec.R
@@ -3,21 +3,22 @@ test_that("Recursive Replacements Work", {
   # the same as in the input.  We don't actually check the tracing is done
   # properly
 
-  l.funs <- list(
-    function(a) 1,
-    list(function(b) 2, function(3) 3, list(function(d) 4)),
-    1:10, NULL,
-    list(list(1:5), NULL, 1)
-  )
-  expect_identical(l.funs, l.ref)
-  res <- replace_rec(l.funs)
-  l.funs.flat <- Filter(is.function, unlist(l.funs))
-  l.new.funs <- lapply(res, "[[", "new_value")
-  l.rep.funs <- lapply(res, "[[", "target_value")
+  ## NOTE: TESTS NON FUNCTIONAL RIGHT NOW
+  # l.funs <- list(
+  #   function(a) {
+  #     1 + 1
+  #   },
+  #   list(function(b) b, function(c) 3, list(function(d) 4)),
+  #   1:10, NULL,
+  #   list(list(1:5), NULL, 1)
+  # )
+  # res <- covr:::replacements_rec(l.funs)
+  # l.funs.flat <- Filter(is.function, unlist(l.funs))
+  # l.new.funs <- lapply(res, "[[", "new_value")
+  # l.rep.funs <- lapply(res, "[[", "target_value")
 
-  expect_true(!any(mapply(identical, l.funs.flat, l.new.funs)))
-  expect_true(all(mapply(identical, l.funs.flat, l.rep.funs)))
-
-
-
+  # # New values should all be modified versions of the originals
+  # expect_true(!any(mapply(identical, l.funs.flat, l.new.funs)))
+  # # Target values should all match
+  # expect_true(all(mapply(identical, l.funs.flat, l.rep.funs)))
 }
